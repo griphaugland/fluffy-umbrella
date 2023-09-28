5713c138c117113c62ae3ed1b3a041a95bf943f3
rydlande <rydlande@hotmail.com>
2023-09-29 00:26:59 +0200

    done with get

--- js/get.js
+++ js/get.js
@@ -7,47 +7,17 @@ const buttonId = document.querySelector("#buttonId");
 const inputName = document.querySelector("#inputName");
 const buttonName = document.querySelector("#buttonName");
 
-/* 
-DENNE FUNGERER LIKT SOM ASYNC FUNKSJONEN "getAll":
-function postData(data) {
-    fetch(api_url, {
-      method: "POST", 
-      body: JSON.stringify(data), 
-      headers:{
-        "Content-Type": "application/json"
-      }
-    })
-    .then(response => response.json())
-    .then(data => console.log(data));
-  }
-const data = {name: "example",
-content: "hei"};
-postData(data); 
-*/
-
-
 //SHOW ALL OBJECTS IN ARRAY FROM URL
 async function getAll(url) {
-    const res = await fetch(url/* , {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    } */)
-    const data = await res.json();
-    console.log(data);
-    showAll(data);
+  const res = await fetch(url);
+  const data = await res.json();
+  showAll(data);
 }
 
 function showAll(items) {
-    // console.log(items);
-
-    container.innerHTML = "";
-    items.forEach((item) => {
-
+  container.innerHTML = "";
+  items.forEach((item) => {
     const { id, name, content } = item;
-    // console.log(id, name, content);
-
     const card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = `
@@ -55,23 +25,20 @@ function showAll(items) {
       <p>ID: ${id}</p>
       <h1>${name}</h1>
       <h3>${content}</h3>
-    </div>`
+    </div>`;
     container.appendChild(card);
-  })
+  });
 }
 
 buttonAll.addEventListener("click", () => {
-  // console.log("clicked"); 
-  getAll(url)
+  getAll(url);
 });
 
-
-//SHOW OBJECT WITH SPECIFIC ID    
+//SHOW OBJECT WITH SPECIFIC ID
 //("unknown" reults means the id does not exists)
 async function getId(url) {
   const id = inputId.value;
-  console.log(id);
-  const res = await fetch(url+id);
+  const res = await fetch(url + id);
   const data = await res.json();
   showId(data);
 }
@@ -79,61 +46,45 @@ async function getId(url) {
 function showId(item) {
   container.innerHTML = "";
   const { id, name, content } = item;
-    // console.log(id, name, content);
-
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
     <div>
       <p>ID: ${id}</p>
       <h1>${name}</h1>
       <h3>${content}</h3>
-    </div>`
-    container.appendChild(card);
-  }
-
+    </div>`;
+  container.appendChild(card);
+}
 
 buttonId.addEventListener("click", (e) => {
-  e.preventDefault();                         //stopper action on form fra HTML. Bruk form for at ENTER er aktivert.
-  getId(url)
-})
+  e.preventDefault();
+  getId(url);
+});
 
-
-//SHOW ALL OBJECTS WITH THE SAME NAME 
+//SHOW ALL OBJECTS WITH THE SAME NAME
 async function getName(urlN) {
   const name = inputName.value;
-  console.log(name);
-  const res = await fetch(urlN+name);
+  const res = await fetch(urlN + name);
   const data = await res.json();
-  showName(data);
+  showAll(data);
 }
 
 function showName(item) {
   container.innerHTML = "";
   const { id, name, content } = item;
-    // console.log(id, name, content);
-
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
     <div>
       <p>ID: ${id}</p>
       <h1>${name}</h1>
       <h3>${content}</h3>
-    </div>`
-    container.appendChild(card);
-  }
-
+    </div>`;
+  container.appendChild(card);
+}
 
 buttonName.addEventListener("click", (e) => {
-  e.preventDefault();                         //stopper action on form fra HTML. Bruk form for at ENTER er aktivert.
-  console.log(urlN);
-  getName(urlN)
-})
-
-
-
-/* 
-/?name=
-/id 
-*/
+  e.preventDefault();
+  getName(urlN);
+});
