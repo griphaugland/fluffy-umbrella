bb2996115457bf94279ee2a26bfc566d2f268721
rydlande <rydlande@hotmail.com>
2023-09-28 14:03:26 +0200

    done with find all, find id, still working on get name

--- js/get.js
+++ js/get.js
@@ -1,6 +1,15 @@
 const url = "https://crud-examples.onrender.com/";
+const urlN = "https://crud-examples.onrender.com/?name=";
+const buttonAll = document.querySelector("#all");
+const container = document.querySelector("#container");
+const inputId = document.querySelector("#inputId");
+const buttonId = document.querySelector("#buttonId");
+const inputName = document.querySelector("#inputName");
+const buttonName = document.querySelector("#buttonName");
 
-/* function postData(data) {
+/* 
+DENNE FUNGERER LIKT SOM ASYNC FUNKSJONEN "getAll":
+function postData(data) {
     fetch(api_url, {
       method: "POST", 
       body: JSON.stringify(data), 
@@ -11,15 +20,13 @@ const url = "https://crud-examples.onrender.com/";
     .then(response => response.json())
     .then(data => console.log(data));
   }
-  
-  const data = {name: "example",
+const data = {name: "example",
 content: "hei"};
-  
-  postData(data); */
-  
-const buttonAll = document.getElementById("all");
-const container = document.getElementById("container");
+postData(data); 
+*/
 
+
+//SHOW ALL OBJECTS IN ARRAY FROM URL
 async function getAll(url) {
     const res = await fetch(url/* , {
       method: "GET",
@@ -32,13 +39,14 @@ async function getAll(url) {
     showAll(data);
 }
 
-async function showAll(items) {
-  console.log(items);
+function showAll(items) {
+    // console.log(items);
 
+    container.innerHTML = "";
     items.forEach((item) => {
 
     const { id, name, content } = item;
-    console.log(id, name, content);
+    // console.log(id, name, content);
 
     const card = document.createElement("div");
     card.classList.add("card");
@@ -53,6 +61,79 @@ async function showAll(items) {
 }
 
 buttonAll.addEventListener("click", () => {
-  console.log("clicked"); 
+  // console.log("clicked"); 
   getAll(url)
 });
+
+
+//SHOW OBJECT WITH SPECIFIC ID    
+//("unknown" reults means the id does not exists)
+async function getId(url) {
+  const id = inputId.value;
+  console.log(id);
+  const res = await fetch(url+id);
+  const data = await res.json();
+  showId(data);
+}
+
+function showId(item) {
+  container.innerHTML = "";
+  const { id, name, content } = item;
+    // console.log(id, name, content);
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <div>
+      <p>ID: ${id}</p>
+      <h1>${name}</h1>
+      <h3>${content}</h3>
+    </div>`
+    container.appendChild(card);
+  }
+
+
+buttonId.addEventListener("click", (e) => {
+  e.preventDefault();                         //stopper action on form fra HTML. Bruk form for at ENTER er aktivert.
+  getId(url)
+})
+
+
+//SHOW ALL OBJECTS WITH THE SAME NAME 
+async function getName(urlN) {
+  const name = inputName.value;
+  console.log(name);
+  const res = await fetch(urlN+name);
+  const data = await res.json();
+  showName(data);
+}
+
+function showName(item) {
+  container.innerHTML = "";
+  const { id, name, content } = item;
+    // console.log(id, name, content);
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <div>
+      <p>ID: ${id}</p>
+      <h1>${name}</h1>
+      <h3>${content}</h3>
+    </div>`
+    container.appendChild(card);
+  }
+
+
+buttonName.addEventListener("click", (e) => {
+  e.preventDefault();                         //stopper action on form fra HTML. Bruk form for at ENTER er aktivert.
+  console.log(urlN);
+  getName(urlN)
+})
+
+
+
+/* 
+/?name=
+/id 
+*/
